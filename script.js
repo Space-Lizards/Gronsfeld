f8650cf680d0d12708f7145dcc1b632f740446cd
ait.creator@mail.ru <ait.creator@mail.ru>
2023-10-19 08:12:45 +0300

    This is a simply crypto algoritm of Gronsfeld. Enjoy :)

--- script.js
+++ script.js
@@ -1,129 +1,71 @@
-function createVigenereSquare() {
-	// Русский алфавит
-	let alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-	// Пустой массив для хранения строк квадрата
-	let square = [];
-	// Цикл по длине алфавита
-	for (let i = 0; i < alphabet.length; i++) {
-		// Получить подстроку алфавита, начиная с i-го символа
-		let substring = alphabet.substring(i);
-		// Добавить оставшуюся часть алфавита в конец подстроки
-		substring += alphabet.substring(0, i);
-		// Добавить подстроку в массив квадрата
-		square.push(substring);
-	}
-	// Вернуть массив квадрата
-	return square;
-}
+function gronsfeldTable ()
+{
+	const buttonCrypt = document.querySelector('#encrypt');
+	const buttonEnrypt = document.querySelector('#decrypt');
+	const alphabet = Array.from('абвгдеёжзийклмнопрстуфхцчшщъыьэюя');
 
-// Функция для шифрования слова по ключу с помощью квадрата Виженера
-function encryptWord(word, key, square) {
-	key = key.replace(/\s/g, '');
-	// Пустая строка для хранения зашифрованного слова
-	let encrypted = '';
-	// Счетчик для индекса буквы ключа
-	let keyIndex = 0;
-	// Цикл по длине слова
-	for (let i = 0; i < word.length; i++) {
-		// Получить i-ю букву слова
-		let letter = word[i];
-		// Проверить, является ли буква пробелом
-		if (letter === ' ') {
-			// Добавить пробел в зашифрованное слово
-			encrypted += ' ';
-		} else {
-			// Получить букву ключа по счетчику
-			let keyLetter = key[keyIndex];
-			// Найти индекс буквы в первой строке квадрата
-			let index = square[0].indexOf(letter);
-			// Найти строку квадрата, соответствующую букве ключа
-			let row = square.find((row) => row[0] === keyLetter);
-			// Получить букву на пересечении строки и столбца
-			let encryptedLetter = row[index];
-			// Добавить зашифрованную букву в строку
-			encrypted += encryptedLetter;
-			// Увеличить счетчик на один
-			keyIndex++;
+	const crypt = (dict, key, word) =>
+	{
+		let cryptWord = '';
+		const keyArray = key.split('').map(char => parseInt(char));
+		let keyIndex = 0;
+		for (let i = 0; i < word.length; i++)
+		{
+			if (dict.includes(word[i]))
+			{
+				cryptWord += dict[(dict.indexOf(word[i]) + keyArray[keyIndex % keyArray.length]) % 33];
+				keyIndex ++;
+			}
+			else
+			{
+				cryptWord += word[i];
+			}
 		}
-	}
-	// Вернуть зашифрованное слово
-	return encrypted;
-}
 
-// Функция для дешифрования слова по ключу с помощью квадрата Виженера
-function decryptWord(word, key, square) {
-	key = key.replace(/\s/g, '');
-	// Пустая строка для хранения расшифрованного слова
-	let decrypted = '';
-	// Счетчик для индекса буквы ключа
-	let keyIndex = 0;
-	// Цикл по длине слова
-	for (let i = 0; i < word.length; i++) {
-		// Получить i-ю букву слова
-		let letter = word[i];
-		// Проверить, является ли буква пробелом
-		if (letter === ' ') {
-			// Добавить пробел в расшифрованное слово
-			decrypted += ' ';
-		} else {
-			// Получить букву ключа по счетчику
-			let keyLetter = key[keyIndex];
-			// Найти строку квадрата, соответствующую букве ключа
-			let row = square.find((row) => row[0] === keyLetter);
-			// Найти индекс буквы в строке квадрата
-			let index = row.indexOf(letter);
-			// Получить букву из первой строки квадрата по индексу
-			let decryptedLetter = square[0][index];
-			// Добавить расшифрованную букву в строку
-			decrypted += decryptedLetter;
-			// Увеличить счетчик на один
-			keyIndex++;
+		return cryptWord;
+	}
+
+	const decrypt = (dict, key, word) =>
+	{
+		let encryptWord = '';
+		const keyArray = key.split('').map(char => parseInt(char));
+		let keyIndex = 0;
+		for (let i = 0; i < word.length; i++)
+		{
+			if (dict.includes(word[i]))
+			{
+				encryptWord += dict[(dict.indexOf(word[i]) - keyArray[keyIndex % keyArray.length]) % 33];
+				keyIndex ++;
+			}
+			else
+			{
+				encryptWord += word[i];
+			}
 		}
+
+		return encryptWord;
 	}
-	// Вернуть расшифрованное слово
-	return decrypted;
+
+	buttonCrypt.addEventListener('click', function()
+	{
+		const result = document.getElementById('result');
+		const key = document.getElementById('key').value;
+		const word = document.getElementById('word').value;
+		
+		result.textContent = `Результат: ${crypt(alphabet, key, word)}`
+	});
+
+	buttonEnrypt.addEventListener('click', function()
+	{
+		const result = document.getElementById('result');
+		const key = document.getElementById('key').value;
+		const word = document.getElementById('word').value;
+
+		result.textContent = `Результат: ${decrypt(alphabet, key, word)}`
+	});
 }
 
-// Создать квадрат Виженера
-let vigenereSquare = createVigenereSquare();
+gronsfeldTable();
 
-// Получить элементы по их id
-let encryptButton = document.getElementById('encrypt');
-let decryptButton = document.getElementById('decrypt');
-let keyInput = document.getElementById('key');
-let wordInput = document.getElementById('word');
-let resultDiv = document.getElementById('result');
 
-// Добавить обработчик события клика на кнопку шифрования
-encryptButton.addEventListener('click', function () {
-	// Получить значение ключа и слова из инпутов
-	let key = keyInput.value.toLowerCase();
-	let word = wordInput.value.toLowerCase();
-	// Проверить, что ключ и слово не пустые и имеют одинаковую длину
-	if (key && word && key.length === word.length) {
-		// Вызвать функцию шифрования и получить результат
-		let encrypted = encryptWord(word, key, vigenereSquare);
-		// Вывести результат в div
-		resultDiv.textContent = 'Зашифрованное слово: ' + encrypted;
-	} else {
-		// Вывести сообщение об ошибке в div
-		resultDiv.textContent = 'Пожалуйста, введите ключ и слово одинаковой длины';
-	}
-});
 
-// Добавить обработчик события клика на кнопку дешифрования
-decryptButton.addEventListener('click', function () {
-	// Получить значение ключа и слова из инпутов
-	let key = keyInput.value.toLowerCase();
-	let word = wordInput.value.toLowerCase();
-	// Проверить, что ключ и слово не пустые и имеют одинаковую длину
-	if (key && word && key.length === word.length) {
-		// Вызвать функцию дешифрования и получить результат
-		let decrypted = decryptWord(word, key, vigenereSquare);
-		// Вывести результат в div
-		resultDiv.textContent = 'Расшифрованное слово: ' + decrypted;
-	} else {
-		// Вывести сообщение об ошибке в div
-		resultDiv.textContent = 'Пожалуйста, введите ключ и слово одинаковой длины';
-	}
-});
